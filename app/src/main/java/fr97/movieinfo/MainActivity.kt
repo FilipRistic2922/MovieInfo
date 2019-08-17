1b1338f01ad95199f4e396c468f3138b0e1e0c77
FR97 <filip.ristic.2922@gmail.com>
2019-08-17 22:07:27 +0200

    Initial Commit

---
+++ app/src/main/java/fr97/movieinfo/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr97.movieinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
